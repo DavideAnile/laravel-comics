bfbd59f79904e01130488d8ff4731272b9c97544
DavideAnile <davide.anile16@gmail.com>
2023-05-09 17:52:53 +0200

    feat : bugfix + main content added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,24 @@
 @section ('content')
 <main>
 
+    <div class="content container">
     @foreach($comics as $singleComic)
+
+    
     <div class="card-container">
 
         <div class="thumb-container">
-            <img :src= "{{$singleComic['thumb']}}" alt="Immagine thumb">
+            <img src= "{{$singleComic['thumb']}}" alt="Immagine thumb">
         </div>
         <div class="cardTitle"> {{ $singleComic['title'] }} </div>
     
     </div>
     @endforeach
+
+    <div class="btn"><button>LOAD MORE</button></div>
+    </div>
+
+    
 
 </main>
 @endsection
